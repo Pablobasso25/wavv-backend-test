c3a2021b15e8b26140ea87fc756583eaa06d41fc
Pablo Basso <pablobasso25@gmail.com>
2026-03-03 19:38:11 -0300

    corrige playlist-routes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -4,17 +4,16 @@ import {
   addSongToPlaylist,
   removeSongFromPlaylist,
   getUserPlaylist,
+  cleanPlaylist,
 } from "../controllers/playlist.controller.js";
-import { validateSchema } from "../middlewares/validator.middleware.js";
-import { addSongSchema } from "../schemas/playlist.schema.js";
 
 const router = Router();
 router.post(
   "/playlist/add",
   authRequired,
-  // validateSchema(addSongSchema), // Temporalmente deshabilitado
   addSongToPlaylist,
 );
 router.get("/playlist", authRequired, getUserPlaylist);
+router.post("/playlist/clean", authRequired, cleanPlaylist);
 router.delete("/playlist/:songId", authRequired, removeSongFromPlaylist);
 export default router;
